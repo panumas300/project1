0d7fc01ae7dfc49044db6ade16fcbb103e4c18dc
INE-02 <INE-02@Admins-iMac-5.local>
2020-07-14 16:50:02 +0700

    test push 2

---
+++ printstring.py
@@ -0,0 +1,12 @@
+print('Hello World')
+print('Information and Network Engineering')
+print('Hello Panumas you will be great')
+print("I'm hungry")
+print('He ask me \"Who got the book?\"')
+print('/\\')
+print('''\ta\na\ta\ta\n\ta''')
+
+
+print('''\ta
+a\ta\ta
+\ta''')
